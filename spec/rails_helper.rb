2d0e87437a87d7e50dd051edf4895d3f85d0b40d
kaa <artemasmith@gmail.com>
2016-10-24 12:04:52 +0500

    travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'capybara/webkit'
 # require 'rspec/retry'
 # require 'capybara/poltergeist'
 Capybara.javascript_driver = :webkit
+Capybara.default_wait_time = 15
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
